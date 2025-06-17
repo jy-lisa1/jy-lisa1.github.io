810dfb74f718805c53ce39d488e41127fd178e0a
Lisa Zhang <jy.lisa1@gmail.com>
2025-06-17 12:51:36 -0400

    fix imports

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,9 +1,7 @@
 import "@fontsource/oxygen/300.css";
 import "@fontsource/oxygen/400.css";
 import "@fontsource/oxygen/700.css";
-import "@fontsource/josefin-sans/300.css";
-import "@fontsource/josefin-sans/400.css";
-import "@fontsource/josefin-sans/700.css";
+import "@fontsource-variable/josefin-sans/index.css";
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
 let theme = createTheme({
