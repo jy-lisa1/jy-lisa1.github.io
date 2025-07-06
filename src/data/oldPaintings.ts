c4619415d710fd69787db6d0ff9b021ed717f749
Lisa Zhang <jy.lisa1@gmail.com>
2025-07-06 14:21:31 -0400

    update notes

--- src/data/oldPaintings.ts
+++ src/data/oldPaintings.ts
@@ -1,13 +1,18 @@
 import type { GalleryItem } from "./galleryItem";
 
-const HIGH_SCHOOL = "Some time in high school";
+const HIGH_SCHOOL = "Some time in high school (2016-2020)";
+const MIDDLE_SCHOOL = "Some time in middle school (2014-2016)";
+const OIL = "Oil";
+const ACRYLIC = "Acrylic";
+const GOUACHE = "Gouache";
+const POTTERY = "Pottery";
 
 export const oldPaintings: GalleryItem[] = [
   {
     path: "/old_paintings/first_oil_painting.jpg",
     title: "My first oil painting!",
     date: HIGH_SCHOOL,
-    medium: "Oil",
+    medium: OIL,
     referenceSource: "Followed a tutorial from an old art book.",
     size: "14 x 18",
     notes: `My first time trying oil painting! It's really different from acrylic and it was hard
@@ -18,7 +23,7 @@ export const oldPaintings: GalleryItem[] = [
     path: "/old_paintings/impressionism.jpg",
     title: "A shot at impressionism",
     date: HIGH_SCHOOL,
-    medium: "Oil",
+    medium: OIL,
     referenceSource: "Nothing! The water lilies are inspired by monet though.",
     size: "16 x 20",
     notes: `Painted over a painting I hated, and decided to give impressionism a try. 
@@ -28,57 +33,58 @@ export const oldPaintings: GalleryItem[] = [
     path: "/old_paintings/venice.jpg",
     title: "Venice",
     date: "July 2024",
-    medium: "Oil",
+    medium: OIL,
     referenceSource: "My camera album after my 2019 Europe trip",
     size: "20 x 24",
-    notes: `Venice was my favourite part of Italy, since I went during a heat wave and it was the 
+    notes: `Venice was my favourite part of my Italy trip. I went during a heat wave, and it was the 
         only place I visited where it wasn't 40 degrees.`,
   },
   {
     path: "/old_paintings/flowers_and_grapes.jpg",
     title: "Flowers",
     date: HIGH_SCHOOL,
-    medium: "Oil",
+    medium: OIL,
     referenceSource:
       "Google Images - It was a popular reference photo, but I can't find it anymore.",
     size: "16 x 20",
-    notes: `I just remember this one being kinda tedious. I like landscapes more :)`,
+    notes: `I just remember this one being kinda tedious. Something about this piece looks weird, but I 
+    can't put my finger on it. I definitely like landscapes more :)`,
   },
   {
     path: "/old_paintings/cranes_dancing.jpg",
     title: "Dancing Cranes",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
+    medium: ACRYLIC,
     referenceSource: "Google Images. It was another popular reference photo that disappeared.",
     size: "20 x 24",
-    notes: `Really procrastinated finishing this one. It sat in the basement, incomplete and 
-        neglected, for years.`,
+    notes: `I lvoed the reference photo, but I really procrastinated finishing this one. 
+    It sat, unfinished and neglected in the basement, for years.`,
   },
   {
     path: "/old_paintings/ghibli_column.jpg",
     title: "Ghibli Courtyard",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
+    medium: ACRYLIC,
     referenceSource: "Pinterest",
     size: "12 x 12",
-    notes: `Really procrastinated finishing this one. It sat in the basement, incomplete and 
-        neglected, for years.`,
+    notes: `One of my favourite recreations! I love how the water turned out - it was pretty hard and time 
+    consuming to achieve this effect with acrylics, but I hadn't learned oil at the time.`,
   },
   {
     path: "/old_paintings/plant_leaves.jpg",
     title: "Plants",
     date: HIGH_SCHOOL,
-    medium: "Oil",
+    medium: OIL,
     referenceSource: "Some home decor I saw",
     size: "20 x 24",
-    notes: `Really procrastinated finishing this one. It sat in the basement, incomplete and 
-        neglected, for years.`,
+    notes: `I saw this as a print at a home decor store, but it was morbidly overpriced. Decided to make 
+    it myself and it actually didn't take long at all - finished it in just one afternoon.`,
   },
   {
     path: "/old_paintings/cranes.jpg",
     title: "Cranes",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
+    medium: ACRYLIC,
     referenceSource: "Google Images - I can't find the original photo anymore.",
     size: "14 x 18",
     notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
@@ -86,65 +92,70 @@ export const oldPaintings: GalleryItem[] = [
   {
     path: "/old_paintings/italy.jpg",
     title: "Italy",
-    date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    date: MIDDLE_SCHOOL,
+    medium: OIL,
+    referenceSource: "A book about Italy",
+    size: "20 x 24",
+    notes: `Not much to say about this one - saw a photo I liked in a book about Italy and decided to 
+    paint it. I never varnished it because I didn't know I was supposed to, and unfortunately it's starting 
+    to crack already :(`,
   },
   {
     path: "/old_paintings/ghibli_gazebo.jpg",
     title: "Ghibli - scene from Porco Rosso",
-    date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    date: MIDDLE_SCHOOL,
+    medium: ACRYLIC,
+    referenceSource: "Ghibli movie",
+    size: "16 x 20",
+    notes: `I actually really liked this one, even though the clouds are looking kinda rough. The red gazebo roof, 
+    combined with the vibrant colours of the garden, made it really fun to look at.`,
   },
 
   {
     path: "/old_paintings/sheep_on_hill.jpg",
     title: "Sheep by the sea",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    medium: GOUACHE,
+    referenceSource: "Pinterest",
+    size: "4 x 6",
+    notes: `A small painting that I hung in my tiny Montreal apartment.`,
   },
   {
     path: "/old_paintings/autumn.jpg",
     title: "A fall scene",
-    date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    date: MIDDLE_SCHOOL,
+    medium: ACRYLIC,
+    referenceSource: "An old art book",
+    size: "16 x 20",
+    notes: `I think I made this one sometime in middle school. I followed a tutorial in an old art book, 
+    and loved how it turned out. It was a lot of fun dabbing my brush onto the canvas to make the leaves.`,
   },
   {
     path: "/old_paintings/sheep_by_water.jpg",
     title: "Another sheep",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    medium: GOUACHE,
+    referenceSource: "Pinterest",
+    size: "4 x 6",
+    notes: `I like painting sheep because my name in Chinese (Yang) is a homonym for sheep/goats.`,
   },
   {
     path: "/old_paintings/sheep_plate.jpg",
     title: "Sheep plate",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    medium: POTTERY,
+    referenceSource: "Pinterest",
+    size: "15-ish cm across",
+    notes: `Painted this at Art Haven. I honestly liked the colours better before the piece was fired in
+    the oven, but it still looks pretty cute. I'm using it to display some of my perfumes right now.`,
   },
   {
     path: "/old_paintings/ghibli_yard.jpg",
     title: "Another Ghibli scene",
     date: HIGH_SCHOOL,
-    medium: "Acrylic",
-    referenceSource: "Google Images - I can't find the original photo anymore.",
-    size: "14 x 18",
-    notes: `I finished this one in one sitting! It's one of my dad's favourites.`,
+    medium: ACRYLIC,
+    referenceSource: "Ghibli movie.",
+    size: "12 x 12",
+    notes: `Not a huge fan of this one. I painted it to match the other square Ghibli scene.`,
   },
 ];
--- src/data/recentPaintings.ts
+++ src/data/recentPaintings.ts
@@ -9,8 +9,10 @@ export const recentPaintings: GalleryItem[] = [
     referenceSource: "US Lighthouses",
     referenceLink: "https://www.us-lighthouses.com/curtis-island-lighthouse",
     size: "16 x 20",
-    notes:
-      "Reminds me of a lighthouse I saw around Vancouver Island. I took a really similar picture when I went on a BC Ferry.",
+    notes: `Reminds me of a lighthouse I saw around Vancouver Island, where I took a really similar 
+      picture when I was on a BC Ferry. I bought a set of mop brushes for this piece so I can blend the sky better, 
+      and I love how smooth it looks in comparison to my previous paintings. I also used the pallet knife trick 
+      on the rocks.`,
   },
   {
     path: "/recent_paintings/cat_by_the_sea.jpg",
@@ -20,8 +22,10 @@ export const recentPaintings: GalleryItem[] = [
     referenceSource: "Pinterest",
     referenceLink: "https://ca.pinterest.com/pin/11540542793846762/",
     size: "16 x 20",
-    notes:
-      "Had a lot of fun with this one! I saw a trick on TikTok to use a pallet knife to create the texture on the rocks, and I think it worked pretty well for my first try. This one only took 6 hours to complete!",
+    notes: `Had a lot of fun with this one! I really wanted to try out a trick I saw on TikTok, where they used
+      a pallet knife to create the texture on the rocks. I was panicking at first because it wasn't working, but
+      after a lot of trial and error, I think it worked out pretty well in the end. This one only took 
+      6 hours to complete! Usually a painting of this size takes me at least a whole weekend.`,
   },
   {
     path: "/recent_paintings/purple_clouds.jpg",
@@ -31,8 +35,11 @@ export const recentPaintings: GalleryItem[] = [
     referenceSource: "Pinterest",
     referenceLink: "https://ca.pinterest.com/pin/300967187622135506/",
     size: "16 x 20",
-    notes:
-      "My first painting of 2025 - I love the way the sunlight glows in the orange part of the clouds.",
+    notes: `My first painting of 2025 - I love the way the sunlight glows in the orange part of the clouds. 
+      I haven't painted water in years, and I forgot how satisfying it feels watching it all come together!
+      This was also my first time using linseed oil to dilute the paints, and it made my life SO MUCH EASIER. The
+      paint glides smoothly over the canvas like soft butter on toast, and I no longer need to fight for my life 
+      trying to get it to behave the way I want it to.`,
   },
   {
     path: "/recent_paintings/ghibli_sunset.jpg",
@@ -43,8 +50,9 @@ export const recentPaintings: GalleryItem[] = [
     referenceLink: "https://ca.pinterest.com/pin/300967187622135515/",
     size: "16 x 20",
     notes: `My first painting in almost 4 years! I was too lazy and unmotivated to make anything 
-        during uni. This scene is apparently from Nausicaa Valley of the Wind - I loved the movie, 
-        but I don't remember it. I decided to recreate this painting instead of making something original to ease 
-        my transition into doing art again after my 4 year gap.`,
+        during uni. I decided to recreate this painting instead of making something original to ease my 
+        transition into doing art again after my 4 year gap. Replicating paintings also helps me learn new 
+        techniques and styles, which is primarily how I learned to paint when I was a kid. This scene is 
+        apparently from Nausicaa Valley of the Wind - I loved the movie, but I don't remember this specific shot.`,
   },
 ];
